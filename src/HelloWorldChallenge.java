ccdd00739f59934eff0e8f30bcc5e968b0785cb5
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-03-31 23:11:37 -0400

    First program in masterclassjavafoundationsjava repository

---
+++ src/HelloWorldChallenge.java
@@ -0,0 +1,8 @@
+// This program was created on March 31, 2021
+
+public class HelloWorldChallenge {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Hemanth");
+    }
+}
+
