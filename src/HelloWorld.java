7bc9c005afde53c31bf74d091140ac67a64b919c
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-03-31 22:03:18 -0400

    First program in masterclassjavafoundationsjava repository

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+// This program was created on March 31, 2021
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World. This is the first program in my masterclassfoundationsjava repository");
+    }
+}
