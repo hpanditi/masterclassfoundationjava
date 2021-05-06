64ede6e8c53b8b8adcca7b992de0556a3f4f6f1a
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-05-05 20:12:19 -0400

    Programs about primitive types and operators

---
+++ src/operatorsone.java
@@ -0,0 +1,34 @@
+// This program was created on May 5, 2021
+
+public class operatorsone {
+
+    public static void main(String[] args) {
+
+        int result = 2 + 3; // 2 + 3 = 5
+
+        System.out.println("2 + 3 = " + result);
+
+        int previousResult = result;
+
+        System.out.println("previousResult = " + previousResult);
+
+        result = result - 1; // 5 - 1 = 4
+
+        System.out.println("5 - 1 = " + result);
+
+        System.out.println("previousResult = " + previousResult);
+
+        result = result * 10; // 4 * 10 = 20
+
+        System.out.println("4 * 10 = " + result);
+
+        result = result / 5; //40 / 5 = 4
+
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3; //
+
+        System.out.println("result % 3 = " + result);
+
+    }
+}
