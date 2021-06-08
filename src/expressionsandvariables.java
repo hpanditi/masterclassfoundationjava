4b10c1493b7db9abde0fd5fea862d2bbef57687e
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-06-08 19:44:08 -0400

    Added programs

---
+++ src/expressionsandvariables.java
@@ -0,0 +1,30 @@
+public class expressionsandvariables {
+
+    public static void main(String[] args) {
+
+        String a = "Hello";
+
+        System.out.println(a);
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        int mySecondNumber = 12;
+
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+
+        System.out.println(mySecondNumber);
+
+        System.out.println(myThirdNumber);
+
+        System.out.println(myTotal);
+
+        System.out.println(myLastOne);
+
+    }
+}
---
+++ src/variablestwo.java
@@ -0,0 +1,14 @@
+public class variablestwo {
+
+    public static void main(String[] args) {
+
+        String a = "Hello";
+
+        System.out.println(a);
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        System.out.println(myFirstNumber);
+
+    }
+}
