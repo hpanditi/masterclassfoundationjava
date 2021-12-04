e9bd8f9f8676d0f43ee8847a9abdb065ece3032c
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-12-04 14:14:31 -0500

    added programs

---
+++ src/ExcessiveComments.java
@@ -0,0 +1,47 @@
+// This is a program to illustrate the excessive use of comments
+
+public class ExcessiveComments {
+    public static void main(String[] args) {
+
+        /*
+       The code below wil print the words 'Hello World' to the screen,
+       and it is fantastic.
+         */
+
+        System.out.println("Hello World");
+
+        /*
+        The code below will assign the value "John" to a String.
+         */
+
+        String name = "John";
+        System.out.println(name);
+
+        /*
+        The code below will assign the value "15" to a integer.
+         */
+
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+
+        /*
+        The code below shows a simple while loop.
+         */
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+
+        /*
+        The code below shows the addition of a first name and last name.
+         */
+
+        String firstName = "Harry";
+        String lastName = "Potter";
+        String fullname = firstName + lastName;
+            System.out.println(fullname);
+        }
+    }
+}
